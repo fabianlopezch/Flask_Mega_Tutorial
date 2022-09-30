9b37407f421306f5e8efc47ff724a2fabbf1217f
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-09-29 23:35:44 -0500

    Defining the SECRET_KEY configuration variable

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ import os
 
 class Config(object):
 	""" This class stores configuration variables """
+
+	# Defining the SECRET_KEY configuration varibale. 
+	# Flask and some of its extensions use the value of the secret key as a cryptographic
+	# 	key, useful to generate signatures or tokens.
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
