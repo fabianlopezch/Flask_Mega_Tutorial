53868719388bdd1387c67aa92d47b0315700504b
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-09-29 23:53:27 -0500

    Applying Flask configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 # We create the application object "app" as an instance of the class Flask imported from the flask package.
 from flask import Flask
+from config import Config	# Import the Config class
 
-app = Flask(__name__)	# app variable inside the app package
+app = Flask(__name__)	# Define the variable app inside the app package
+app.config.from_object(Config)	# Apply the configurations
 
 from app import routes	# We import the routes module (from app package) at this point because it requires the app variable to be imported
