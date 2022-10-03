6a014885a3e476e4425ea85c3052589ac1d331c3
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-10-03 18:08:22 -0500

    Initilize Flask-SQLAlchemy and Flask-Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 # We create the application object "app" as an instance of the class Flask imported from the flask package.
 from flask import Flask
 from config import Config	# Import the Config class
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)	# Define the variable app inside the app package
 app.config.from_object(Config)	# Apply the configurations
+db = SQLAlchemy(app)	# Create a database instance
+migrate = Migrate(app, db)	# Create a database migration engine instance
 
-from app import routes	# We import the routes module (from app package) at this point because it requires the app variable to be imported
+from app import routes, models	# We import the routes module (from app package) at this point because it requires the app variable to be imported
+								# Import the module 'models' in which the structure of the database is defined
