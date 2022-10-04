f30fd1e50e6813dd2b792220a8515143f6a47f1d
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-10-04 17:29:55 -0500

    Configuring the user loader function

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from app import login
 
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -29,3 +30,7 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return '<Post {}>'. format(self.body)
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
