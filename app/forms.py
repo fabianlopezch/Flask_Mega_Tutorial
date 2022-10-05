8a703f746652372b742d2d1e51a7268ba361ee8f
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-10-04 23:11:11 -0500

    [Fix] Creating the user registration web form class

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, equalTo
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
@@ -12,7 +12,7 @@ class RegistrationForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
-	password2 = PasswordField('Repeat Password', validators=[DataRequired(), equalTo('password')])
+	password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Register')
 
 	def validate_username(self, username):
